9d46dcf7ae1e509fa412e407b87bf34a62227356
haya <haya@appurunoMacBook-puro.local>
2020-05-21 17:16:39 +0900

    フォロー機能完成！！！

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @follows = @user.followings
+    @followers = @user.followers
   end
 
   def follows
